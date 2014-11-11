634d031c4bac35c0dcc34782c5aac8a2007222df
Matti Paksula <matti.paksula@iki.fi>
2014-11-11 14:14:38 +0200

    added reminder

--- init.rb
+++ init.rb
@@ -6,6 +6,8 @@ files=[
 ".zshrc"
 ]
 
+puts "install oh-my-zsh, nvm, rvm, brew + brew cask packages, scm breeze before running this"
+gets
 
 for file in files do
   system("rm #{ENV['HOME']}/#{file}")
